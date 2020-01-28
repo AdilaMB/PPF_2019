aa0698233fe02befdaac5866adee6fe26161c02d
AdilaMB <42074986+AdilaMB@users.noreply.github.com>
2020-01-28 10:07:31 -0300

    Update

--- appdigilib/models.py
+++ appdigilib/models.py
@@ -2,13 +2,8 @@
 Create by abarrio
 Date: 08/04/2019
 """
-from statistics import mode
-from xmlrpc.client import DateTime
-from django.db import models
-from django.utils.timezone import now
-from datetime import date
-from django.core.validators import MinValueValidator
 
+from django.db import models
 
 # Model for category entity
 class Category(models.Model):
